eb7dbd72ffc483f5b62e520357d5d16cec61c4ef
Nathan Merigeau <nathan.merigeau@u-psud.fr>
2023-01-26 17:30:33 +0100

    qqsdqds

--- Assets/InventoryUI.cs
+++ Assets/InventoryUI.cs
@@ -11,21 +11,27 @@ public class InventoryUI : MonoBehaviour
 
     void Start()
     {
-        Canvas canvas = transform.GetChild(0).gameObject.GetComponent<Canvas>() ;
+        Transform contents = gameObject.transform.GetChild(0).GetChild(0).GetChild(0);
+        //HorizontalLayoutGroup hlg = contents.GetComponent<HorizontalLayoutGroup>();
+
+        Vector2 itemSize = new Vector2(50,50);
+
         for (int i=0;i<inventory.items.Count;i++)
         {
             ItemType it = inventory.items[i];
 
             Image img = new GameObject("item").AddComponent<Image>();
-            Texture2D textureItem = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Resources/test.png");
+            Texture2D textureItem = (Texture2D)Resources.Load("test");
+
             Sprite spriteItem = Sprite.Create(textureItem, new Rect(0, 0, textureItem.width, textureItem.height), new Vector2(0.5f, 0.5f));
             img.sprite = spriteItem;
 
-            img.transform.SetParent(canvas.transform);
+            img.transform.SetParent(contents);
 
-            img.transform.localPosition = Vector3.zero;
-            SpriteRenderer spriteRenderer = img.GetComponent<SpriteRenderer>();
-            spriteRenderer.size = new Vector2(7, 2);
+            img.transform.localPosition = new Vector3(img.transform.localPosition.x, img.transform.localPosition.y, 0f);
+            img.transform.localScale = Vector3.one;
+            img.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            img.rectTransform.sizeDelta = itemSize;
         }
     }
 
