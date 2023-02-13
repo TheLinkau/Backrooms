0f8995bad24a3c84529db43a19a6ce420aa17277
Nathan Merigeau <nathan.merigeau@u-psud.fr>
2023-02-13 17:28:17 +0100

    inv ui plutot ok

--- Assets/InventoryUI.cs
+++ Assets/InventoryUI.cs
@@ -3,32 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.UI;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 using static Inventory;
+using System;
 
 public class InventoryUI : MonoBehaviour
 {
-    public Inventory inventory;
+    public XRNode controller;
+    public Player player;
+
+    private Vector2 inputAxis;
+    private float radPerItem;
+    private float startRad;
+    private float radius;
+
+    private Texture2D textureItemSelected;
+    private Image imgItemSelected;
 
     void Start()
     {
-        Transform contents = gameObject.transform.GetChild(0).GetChild(0).GetChild(0);
-        //HorizontalLayoutGroup hlg = contents.GetComponent<HorizontalLayoutGroup>();
+        Transform contents = gameObject.transform;
+        Vector2 itemSize = new Vector2(1, 1);
 
-        Vector2 itemSize = new Vector2(50,50);
+        radPerItem = (2*Mathf.PI) / player.inventory.items.Count;
+        startRad = Mathf.PI / 2.0f;
+        radius = 1.75f;
 
-        for (int i=0;i<inventory.items.Count;i++)
+        textureItemSelected = (Texture2D)Resources.Load("selected");
+        imgItemSelected = new GameObject("selected").AddComponent<Image>();
+        imgItemSelected.sprite = Sprite.Create(textureItemSelected, new Rect(0, 0, textureItemSelected.width, textureItemSelected.height), new Vector2(0.5f, 0.5f));
+        imgItemSelected.transform.SetParent(contents);
+        imgItemSelected.transform.localScale = Vector3.one;
+        imgItemSelected.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        imgItemSelected.rectTransform.sizeDelta = itemSize;
+
+        for (int i=0;i< player.inventory.items.Count;i++)
         {
-            ItemType it = inventory.items[i];
+            ItemType it = player.inventory.items[i];
+            float x = radius * Mathf.Cos(startRad + i * radPerItem);
+            float y = radius * Mathf.Sin(startRad + i * radPerItem);
 
-            Image img = new GameObject("item").AddComponent<Image>();
+            if (i == player.itemSelected)
+            {
+                imgItemSelected.transform.localPosition = new Vector3(x, y, 0f);
+            }
+
             Texture2D textureItem = (Texture2D)Resources.Load("test");
 
-            Sprite spriteItem = Sprite.Create(textureItem, new Rect(0, 0, textureItem.width, textureItem.height), new Vector2(0.5f, 0.5f));
-            img.sprite = spriteItem;
-
+            Image img = new GameObject("item").AddComponent<Image>();
+            img.sprite = Sprite.Create(textureItem, new Rect(0, 0, textureItem.width, textureItem.height), new Vector2(0.5f, 0.5f));
             img.transform.SetParent(contents);
-
-            img.transform.localPosition = new Vector3(img.transform.localPosition.x, img.transform.localPosition.y, 0f);
+            img.transform.SetAsFirstSibling();
+            img.transform.localPosition = new Vector3(x, y, 0f);
             img.transform.localScale = Vector3.one;
             img.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
             img.rectTransform.sizeDelta = itemSize;
@@ -38,6 +65,33 @@ public class InventoryUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        InputDevice device = InputDevices.GetDeviceAtXRNode(controller);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+
+        if(inputAxis.x == 0f && inputAxis.y == 0f) return;
+
+        //Text text = GameObject.Find("Text").GetComponent<Text>();
+        //text.text = menuPressed.ToString();
+
+        inputAxis.x = -inputAxis.x;
+
+        float rad = Mathf.Atan2(inputAxis.y, inputAxis.x);
+        if(rad < 0f)
+        {
+            rad = Mathf.PI + Mathf.PI + rad;
+        }
+
+        float radPerItem = (2 * Mathf.PI) / player.inventory.items.Count;
+        int index = (int)Math.Round(rad/radPerItem) - 1;
+        if(index < -1)
+        {
+            index = player.inventory.items.Count - index;
+        }
+
+        player.itemSelected = index;
+
+        float x = radius * Mathf.Cos(startRad + index * radPerItem);
+        float y = radius * Mathf.Sin(startRad + index * radPerItem);
+        imgItemSelected.transform.localPosition = new Vector3(x, y, 0f);
     }
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,11 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR;
+
+[System.Serializable]
+public class MenuButtonEvent : UnityEvent<bool> { }
 
 public class Player : MonoBehaviour
 {
+    public XRNode controller;
+
+    public MenuButtonEvent menuButtonPress;
+    private bool lastButtonState = false;
+
     public Inventory inventory;
-    public int life;
+    public int itemSelected;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +29,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        InputDevice device = InputDevices.GetDeviceAtXRNode(controller);
+        bool state;
+        device.TryGetFeatureValue(CommonUsages.menuButton, out state);
+
+        if (state != lastButtonState)
+        {
+            menuButtonPress.Invoke(state);
+            lastButtonState = state;
+        }
     }
 }
